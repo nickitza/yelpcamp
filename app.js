686dcce3d0248f6b26de23154912c3865fd52711
Nicki Nixon <nickitza@gmail.com>
2019-08-05 13:37:30 -0600

    conditional login/logout/register nav

--- app.js
+++ app.js
@@ -14,19 +14,22 @@ var express = require("express"),
     app.set("view engine", "ejs")
     app.use(express.static(__dirname + "/public"))
     seedDB()
-
     app.use(require("express-session")({
       secret: "Eric is a babe",
       resave: false,
       saveUninitialized: false
     }))
-
     app.use(passport.initialize())
     app.use(passport.session())
     passport.use(new LocalStrategy(User.authenticate()))
     passport.serializeUser(User.serializeUser())
     passport.deserializeUser(User.deserializeUser())
+    app.use(function(req,res,next){
+      res.locals.currentUser = req.user
+      next()
+    })
 
+//* INDEX
 app.get('/', function(req, res){
   res.render("landing")
 })
@@ -36,7 +39,7 @@ app.get('/campgrounds', function(req, res){
     if(err){
       console.log("**ERR: " + err)
     }else{
-      res.render("campgrounds/index", {campgrounds: allCamps})
+      res.render("campgrounds/index", {campgrounds: allCamps, currentUser: req.user})
     }
   })
 })
