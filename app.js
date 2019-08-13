891b643011e715de5b472a87f7dabab3c0f453d7
Nicki Nixon <nickitza@gmail.com>
2019-08-12 20:33:07 -0600

    test ip

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ var indexRoutes = require("./routes/index")
   app.use("/campgrounds", campgroundRoutes)
 
 // =========================================================================
-app.listen(3000, function(){
+app.listen(3000, process.env.IP, function(){
   console.log("** Yelp Camp server started **")
 })
